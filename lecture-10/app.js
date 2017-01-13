9e404f809904d8d9c61c5689bbcc9755d276710f
getvivekjoshi <getvivekjoshi@gmail.com>
2017-01-13 15:22:01 +0530

    lecture-11

---
+++ lecture-10/app.js
@@ -0,0 +1,18 @@
+/*(function(){
+  'use strict';
+angular.module('DIApp', [])
+.controller('DIController', DIController);
+
+DIController.$inject = ['$scope', '$filter'];
+function DIController($scope, $filter) {
+  $scope.name = "Vivek";
+
+  $scope.upper = function () {
+    var upCase = $filter ('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+}
+})();*/
+
+
+!function(){"use strict";function e(e,n){e.name="Vivek",e.upper=function(){var r=n("uppercase");e.name=r(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
